5687f5aa5c6c75401b03d918fd51e7d7af647531
David Brunow <david.brunow@me.com>
2013-07-12 09:06:23 -0500

    Initial Commit

---
+++ Mission DharmaTests/Mission_DharmaTests.m
@@ -0,0 +1,36 @@
+//
+//  Mission_DharmaTests.m
+//  Mission DharmaTests
+//
+//  Created by David Brunow on 7/12/13.
+//  Copyright (c) 2013 David Brunow. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Mission_DharmaTests : XCTestCase
+
+@end
+
+@implementation Mission_DharmaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
