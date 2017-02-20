1ff4a432d0f53cabb3c1811ca8cc9dab87d9682c
David Brunow <helloDavid@brunow.org>
2017-02-19 23:22:59 -0600

    Migrate the PodcastEpisode class to Swift

---
+++ PodcastEpisode+CoreDataClass.swift
@@ -0,0 +1,103 @@
+//
+//  PodcastEpisode+CoreDataClass.swift
+//  Mission Dharma
+//
+//  Created by David Brunow on 2/19/17.
+//  Copyright © 2017 David Brunow. All rights reserved.
+//
+/*
+ The MIT License (MIT)
+ 
+ Copyright (c) 2017 David Brunow
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import Foundation
+import CoreData
+
+@objc(PodcastEpisode)
+public class PodcastEpisode: NSManagedObject
+{
+    var isDownloaded = false
+    var downloadProgress = Float(0)
+    var cacheFolderPath = ""
+    
+    public override func awakeFromFetch()
+    {
+        super.awakeFromFetch()
+        
+        if let this = localPathString
+        {
+            cacheFolderPath = "\(AppDelegate.applicationHome)/\(this)"
+            
+            if FileManager.default.fileExists(atPath: cacheFolderPath)
+            {
+                isDownloaded = true
+                try? FileManager.default.addSkipBackupAttributeToItemAtURL(url: URL(fileURLWithPath: cacheFolderPath))
+            }
+            else
+            {
+                isDownloaded = false
+            }
+        }
+    }
+    
+    func download(completionHandler: @escaping (_ error: Error?) -> ())
+    {
+        if let this = urlString
+            , let thisLocalPathString = localPathString
+        {
+            cacheFolderPath = "\(AppDelegate.applicationHome)/\(thisLocalPathString)"
+                
+            if let url = URL(string: this)
+            {
+                ApiClient.sharedInstance.makeApiRequest(forUrl: url, withMethod: .get, andBody: nil)
+                { data, response, error in
+                    if let thisData = data
+                    {
+                        if FileManager.default.createFile(atPath: self.cacheFolderPath, contents: thisData, attributes: nil)
+                        {
+                            self.isDownloaded = true
+                            try? FileManager.default.addSkipBackupAttributeToItemAtURL(url: URL(fileURLWithPath: self.cacheFolderPath))
+                        }
+                        else
+                        {
+                            self.isDownloaded = false
+                        }
+                        
+                        completionHandler(error)
+                    }
+                }
+            }
+        }
+    }
+    
+    func delete()
+    {
+        try? FileManager.default.removeItem(atPath: cacheFolderPath)
+        
+        isDownloaded = false
+    }
+    
+    func save()
+    {
+        try? managedObjectContext?.save()
+    }
+}
+
+extension FileManager
+{
+    func addSkipBackupAttributeToItemAtURL(url: URL) throws
+    {
+        var fileURL = url
+        var resourceValues = URLResourceValues()
+        resourceValues.isExcludedFromBackup = true
+        
+        try? fileURL.setResourceValues(resourceValues)
+    }
+}
--- PodcastEpisode+CoreDataProperties.swift
+++ PodcastEpisode+CoreDataProperties.swift
@@ -1,12 +1,14 @@
 //
-//  DHBPodcastEpisode.h
+//  PodcastEpisode+CoreDataProperties.swift
 //  Mission Dharma
 //
-//  Created by David Brunow on 8/7/13.
+//  Created by David Brunow on 2/19/17.
+//  Copyright © 2017 David Brunow. All rights reserved.
+//
 /*
  The MIT License (MIT)
  
- Copyright (c) 2014 David Brunow
+ Copyright (c) 2017 David Brunow
  
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  
@@ -14,29 +16,24 @@
  
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#import <CoreData/CoreData.h>
-#import <Foundation/Foundation.h>
 
-@interface DHBPodcastEpisode : NSManagedObject
+import Foundation
+import CoreData
 
-@property (nonatomic, strong) NSString *title;
-@property (nonatomic, strong) NSString *speaker;
-@property (nonatomic, strong) NSDate *recordDate;
-@property (nonatomic, strong) NSString *URLString;
-@property (nonatomic, strong) NSString *localPathString;
-@property (nonatomic) NSMutableData *tempEpisodeData;
-@property (nonatomic) float currentPlaybackPosition;
-@property (nonatomic) float duration;
-@property (nonatomic) bool isUnplayed;
-@property (nonatomic) bool isDownloaded;
-@property (nonatomic) float totalFileSize;
-@property (nonatomic) float downloadInProgress;
-@property (nonatomic, strong) NSString *cacheFolderPathString;
+extension PodcastEpisode
+{
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<PodcastEpisode>
+    {
+        return NSFetchRequest<PodcastEpisode>(entityName: "PodcastEpisode");
+    }
 
--(void) downloadEpisode;
--(void) deleteEpisode;
--(void) parseInfo:(NSString *)info;
--(void) save;
+    @NSManaged public var currentPlaybackPosition: NSNumber?
+    @NSManaged public var duration: NSNumber?
+    @NSManaged public var isUnplayed: NSNumber?
+    @NSManaged public var localPathString: String?
+    @NSManaged public var recordDate: Date?
+    @NSManaged public var speaker: String?
+    @NSManaged public var title: String?
+    @NSManaged public var urlString: String?
 
-
-@end
+}
